f0df0e149568b6f93e957f33468bbeb7d78e7421
Antoine <ant.meresse@gmail.com>
2022-12-23 15:29:35 +0100

    feat: select urt path

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -1,3 +1,32 @@
+<script>
+  import { urtPath } from "./urtService";
+  import { open } from "@tauri-apps/api/dialog";
+
+  function deletePath() {
+    urtPath.set("");
+  }
+
+  function selectPath() {
+    open({ multiple: false }).then((filepath) => {
+      if (filepath && !Array.isArray(filepath)) {
+        urtPath.set(filepath);
+      }
+    });
+  }
+</script>
+
 <div>
-  <h1>I'm the settings page</h1>
+  <p>Game Path</p>
+  <input bind:value={$urtPath} disabled />
+  {#if $urtPath.length !== 0}
+    <button on:click={deletePath}>X</button>
+  {:else}
+    <button on:click={selectPath}>Select</button>
+  {/if}
 </div>
+
+<style>
+  input {
+    width: 50vw;
+  }
+</style>
